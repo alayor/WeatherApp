0af598f7bf8878b28fefa725a2d30675ab3405ee
Alonso A. Ortega <alayor3@gmail.com>
2019-03-18 22:33:48 -0700

    chore: add readline dependency and test it on the index.js file.

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const weather = require('weather-js')
 
-weather.find({search: 'San Francisco, CA', degreeType: 'F'}, function (err, result) {
-  if (err) console.log(err);
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
 
-  console.log(JSON.stringify(result, null, 2))
+readline.question(`Want weather?`, (name) => {
+  weather.find({search: 'San Francisco, CA', degreeType: 'F'}, function (err, result) {
+    if (err) console.log(err);
+
+    console.log(JSON.stringify(result, null, 2))
+  })
+
+  readline.close()
 })
