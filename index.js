7fc584a48e00e4d4dd776489ad7ddacd237424bc
Alonso A. Ortega <alayor3@gmail.com>
2019-03-18 22:39:53 -0700

    refactor: promisify weather.find function.

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const weather = require('weather-js')
+const util = require('util')
+const findWeather = util.promisify(weather.find)
 
 const readline = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout
 })
 
-readline.question(`Give me CSV of location names or postal codes (e.g Atlanta, 76051): `, (input) => {
-  weather.find({search: 'San Francisco, CA', degreeType: 'F'}, function (err, result) {
-    if (err) console.log(err);
-
-    console.log(JSON.stringify(result, null, 2))
-  })
-
+readline.question(`Give me CSV of location names or postal codes (e.g Atlanta, 76051): `, async (input) => {
+  const result = await findWeather({search: 'San Francisco, CA', degreeType: 'F'})
+  console.log(JSON.stringify(result, null, 2))
   readline.close()
 })
