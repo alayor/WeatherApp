7a3249f0d0426c2c275798b931bce8412528ece0
Alonso A. Ortega <alayor3@gmail.com>
2019-03-19 09:46:05 -0700

    refactor: Move degree type to a constant.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const findWeather = util.promisify(weather.find)
 const geoTz = require('geo-tz')
 const moment = require('moment-timezone')
 const now = moment();
+const degreeType = 'F'
 
 const readline = require('readline').createInterface({
   input: process.stdin,
@@ -15,14 +16,14 @@ readline.question(`Give me CSV of location names or postal codes (e.g Atlanta, 7
   const locations = input.split(',').map(l => l.trim())
   await Bluebird.each(locations, async location => {
     const weatherAndTime = await getLocationWeatherAndTime(location)
-    console.log(`${weatherAndTime.location}: ${weatherAndTime.weather}F (${weatherAndTime.time})`)
+    console.log(`${weatherAndTime.location}: ${weatherAndTime.weather}${degreeType} (${weatherAndTime.time})`)
   })
 
   readline.close()
 })
 
 async function getLocationWeatherAndTime(location) {
-  const result = await findWeather({search: location, degreeType: 'F'})
+  const result = await findWeather({search: location, degreeType: degreeType})
   const timezone =  geoTz(result[0].location.lat, result[0].location.long)
   const time = now.tz(timezone[0]).format('h:mm a z')
 
