dc3e34484c65b2827b418c56bc85cdd5a976ba32
Alonso A. Ortega <alayor3@gmail.com>
2019-03-19 09:54:22 -0700

    refactor: rearrange require statements.

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const Bluebird = require('bluebird')
 const weather = require('weather-js')
 const util = require('util')
-const findWeather = util.promisify(weather.find)
 const geoTz = require('geo-tz')
 const moment = require('moment-timezone')
-const now = moment();
+
+const now = moment()
 const degreeType = 'F'
+const findWeather = util.promisify(weather.find)
 
 const readline = require('readline').createInterface({
   input: process.stdin,
@@ -24,7 +25,7 @@ readline.question(`Give me CSV of location names or postal codes (e.g Atlanta, 7
 
 async function getLocationWeatherAndTime(location) {
   const result = await findWeather({search: location, degreeType: degreeType})
-  const timezone =  geoTz(result[0].location.lat, result[0].location.long)
+  const timezone = geoTz(result[0].location.lat, result[0].location.long)
   const time = now.tz(timezone[0]).format('h:mm a z')
 
   return {
